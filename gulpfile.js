19dc7dfd5e9b2d7698c2457d495d85a51104cb6b
wfpaisa <wfpaisa@gmail.com>
2017-04-27 18:51:59 -0500

    correct async javascript

--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,134 @@
 var fs = require('fs');
 var path = require('path');
 var gulp = require('gulp');
-var run = require('gulp-run');
 var map = require('map-stream')
 const exec = require('child_process').exec;
 
+/**
+ * Render the object from a SVG file to PNG in a path
+ * 
+ * https://inkscape.org/es/doc/inkscape-man.html
+ *
+ * @param      {string}  FILE_FROM  The file from (?/xx.svg)
+ * @param      {string}  PNG_NAME    The file to PNG (?/xx.png)
+ * @param      {string}  OBJECT_ID  The object id (icon-32)
+ */
+function svgToPng(filePath, objectId, pngName, cb) {
 
-// Render an object from svg file to png
-//
-// Example: 
-// svgToPng('./src/bitmap/apps/demo.svg','./Plane/bitmap/apps/48/demo.png','icon-32')
-//
-// inkscape more info -> https://inkscape.org/es/doc/inkscape-man.html
+	let renderSVG = `inkscape --export-id=${objectId} --export-id-only --export-png=${pngName} "${filePath}" --export-area-snap`;
+	
+	exec(renderSVG, (err, stdout, stderr) => {
+
+		if (err) return cb(err);
+
+		console.log(`Render: ${pngName}`)
+
+		cb();
+	});
+}
+
 
 
 /**
- * Render the object from a SVG file to PNG in a path
+ * Create all possible folder sizes from id name from svg file
  *
- * @param      {string}  FILE_FROM  The file from (?/xx.svg)
- * @param      {string}  FILE_TO    The file to PNG (?/xx.png)
- * @param      {string}  OBJECT_ID  The object id (icon-32)
+ * @param      {obj}       objects   A object with all name ids {icon:64}
+ * @param      {string}    filePath  The file path
+ * @return     {Function}  callback
  */
-function svgToPng(FILE_FROM, FILE_TO, OBJECT_ID) {
-    var renderSVG = `inkscape --export-id=${OBJECT_ID} --export-id-only --export-png=${FILE_TO} ${FILE_FROM}`;
-    exec(renderSVG, (err, stdout, stderr) => {
+function createSizeFolder(objects, filePath, cb) {
 
-        if (err) return err
+	let count = Object.keys(objects).length;
 
-        // console.log(`render: ${stdout}`);
-        console.log(`Render: ${FILE_TO}`)
 
-        return true
-    });
+	// Loop async of create directories
+	function loopasync(cbLoop) {
+
+		// Directory where is the svg file.
+		let fileDir = path.dirname(filePath).split(__dirname + '/src/').pop();
+
+		for (const key in objects) {
+
+			let dirSizeTo 	= `./${fileDir}/${objects[key]}`,
+				fileName 	= path.basename(filePath, '.svg'),
+				pngName 	= key === 'icon' ? `./${fileDir}/${objects[key]}/${fileName}.png` : `./${fileDir}/${objects[key]}/${key}.png`,
+				objectId 	= `${key}_${objects[key]}`;
+
+			// Create directory
+			exec(`mkdir -p ${dirSizeTo}`, (err, stdout, stderr) => {
+
+				if (err) return cbLoop(err);
+				if (stderr) return cbLoop(stderr);
+
+
+				svgToPng(filePath, objectId, pngName, function(err) {
+					if (err) return cbLoop(err);
+					cbLoop();
+				})
+
+			})
+
+		}
+
+	}
+
+	// Check all async
+	loopasync(function(err) {
+
+		if (err) return cb(err);
+
+		// total of objects
+		count--;
+
+		// When it is equal to 0 is because all callbacks have returned
+		if (count === 0) cb();
+	})
+
+
 }
+
+
+
+/**
+ * Gets all objects in a .svg file.
+ *
+ * @param      {string}    filePath  path svg file
+ * @param      {Function}  cb        callback
+ * @return     {err}       The object in svg.
+ */
+function getObjInSvg(filePath, cb) {
+
+	// Only svg files
+	if (path.extname(filePath) != '.svg') return cb();
+
+	var svgFile = `inkscape -S "${filePath}"`;
+
+	// Get all objects from svg file
+	exec(svgFile, (err, stdout, stderr) => {
+
+		if (err) cb(err);
+		if (stderr) cb(stderr);
+
+		var re = /\n(.{2,})_([0-9]{2,}),/g,
+			matches = {};
+
+		stdout.replace(re, (match, $1, $2) => {
+			matches[$1] = $2
+		});
+
+
+		// to create folder sizes
+		createSizeFolder(matches, filePath, function(err) {
+			if (err) return cb(err);
+			cb();
+		})
+
+
+	});
+
+}
+
+
 
 
 /**
@@ -47,84 +143,138 @@ function svgToPng(FILE_FROM, FILE_TO, OBJECT_ID) {
  *
  * @param      {string}  FILE_PATH  The file path (?/xx.svg)
  */
-function renderIcon(FILE_PATH) {
-    var FILE_NAME = path.basename(FILE_PATH, '.svg'),
-        FILE_DIR = path.dirname(FILE_PATH).split(__dirname + '/src/').pop();
-
-    // Only svg files
-    if (path.extname(FILE_PATH) != '.svg') return
-
-    // Obtein all objects from svg icon
-    var GET_OBJECTS_ICON = new Promise(function(resolve, reject) {
-
-        var OBJECTS_ICON = 'inkscape -S ' + FILE_PATH;
-
-        exec(OBJECTS_ICON, (err, stdout, stderr) => {
-
-            if (err) reject(err);
-            if (stderr) reject(stderr);
-
-            // string to array
-            var toarray = stdout.split('\n');
-
-            // filter all items with an icon object
-            var arrayOnlyIcons = toarray.filter(i => i.includes('icon-'))
-
-            resolve(arrayOnlyIcons);
-        });
-
-    })
-
-    GET_OBJECTS_ICON.then(function(OBJECTS) {
+function renderIcon(FILE_PATH, cb) {
 
 
-        OBJECTS.map((OBJECT) => {
+	var FILE_NAME = path.basename(FILE_PATH, '.svg'),
+		FILE_DIR = path.dirname(FILE_PATH).split(__dirname + '/src/').pop();
 
-            var item_name = OBJECT.split(',')[0];
-            var item_size = item_name.replace('icon-','');
-            var item_to = `./${FILE_DIR}/${item_size}/${FILE_NAME}.png`
-            var dir_to = `./${FILE_DIR}/${item_size}`
+	// Only svg files
+	if (path.extname(FILE_PATH) != '.svg') return cb
 
-            // Create directory
-            exec(`mkdir -p ${dir_to}`, (err, stdout, stderr) => {
-                
-                if (err) console.log(err);
-                if (stderr) console.log(stderr);
-                
-                svgToPng(FILE_PATH,item_to,item_name)
-            })
-            
-        })
+	// Obtein all objects from svg icon
+	var GET_OBJECTS_ICON = new Promise(function(resolve, reject) {
 
-    }, function(err) {
+		var OBJECTS_ICON = `inkscape -S "${FILE_PATH}"`;
 
-        console.log(err)
+		// Get all objects from svg file
+		exec(OBJECTS_ICON, (err, stdout, stderr) => {
 
-    })
+			if (err) reject(err);
+			if (stderr) reject(stderr);
+
+			var re = /\n(.{2,})_([0-9]{2,}),/g,
+				matches = {};
+
+			stdout.replace(re, (match, $1, $2) => {
+				matches[$1] = $2
+			});
+
+			resolve(matches);
+		});
+
+	})
+
+	GET_OBJECTS_ICON.then(function(OBJECTS) {
+
+
+		for (const KEY in OBJECTS) {
+
+			var PNG_NAME = KEY === 'icon' ? `./${FILE_DIR}/${OBJECTS[KEY]}/${FILE_NAME}.png` : `./${FILE_DIR}/${OBJECTS[KEY]}/${KEY}.png`,
+				DIR_TO = `./${FILE_DIR}/${OBJECTS[KEY]}`,
+				OBJECT_ID = `${KEY}_${OBJECTS[KEY]}`;
+
+
+			createFolder(FILE_PATH, PNG_NAME, DIR_TO, OBJECT_ID, function(err) {
+				if (err) return cb(err);
+
+				cb()
+			});
+
+		}
+
+		cb(null, OBJECTS);
+
+	}, function(err) {
+
+		cb(err)
+
+	})
 
 }
 
 
-/**
- * Start tasks
- */
-gulp.task('default', function() {
-    gulp.src('./src/**/*')
-        .pipe(map(function(file, callback) {
+/*==========================================
+=            Remove directories            =
+==========================================*/
+gulp.task('clean', function(cb) {
+	// Clean directories
+	exec('rm -R ./plane ./plane-dark && mkdir ./plane ./plane-dark', (err, stdout, stderr) => {
+		cb(null)
+	});
+})
 
-            // Only render files
-            // if (fs.statSync(file.path).isFile()){
 
-                // Only svg files
-                if (fs.statSync(file.path).isFile() &&  (path.extname(file.path) == '.svg')){
-                    renderIcon(file.path);
-                    callback(null, null)
-                }   
+/*==========================================
+=            Create icons                  =
+==========================================*/
+gulp.task('createIcon', ['clean'], function(cb) {
 
-            // }
 
-            callback(null, file)
+	gulp.src('./src/**/*')
+		.pipe(map(function(file, callback) {
 
-        }))
-        .pipe(gulp.dest(__dirname))
+			// Only svg files
+			if (fs.statSync(file.path).isFile() && (path.extname(file.path) == '.svg')) {
+
+				// // Render icons
+				// renderIcon(file.path,function(err){
+				//     if(err) return cb(err)
+
+				//     callback();
+				// });
+
+				// Start process to create icons
+				getObjInSvg(file.path, function(err) {
+					if (err) return callback(err)
+
+					callback();
+				})
+
+				// No copy svg file
+			} else {
+				callback(null, file);
+			}
+		}))
+		.pipe(gulp.dest(__dirname))
+		.on('end', cb);
+
+
+
+})
+
+
+/*==========================================
+=            Copy directories            =
+==========================================*/
+gulp.task('copy', ['createIcon'], function(cb) {
+	// Clean directories
+	// exec('cp -R ./plane /usr/share/icons/plane && cp -R ./plane-dark /usr/share/icons/plane-dark ',(err,stdout,stderr)=>{
+
+
+	//     if (err) console.log(err);
+	//     if (stderr) console.log(stderr);
+
+	//     console.log('-------Copy--------')
+	//     cb(null)
+	// });
+	cb(null);
+})
+
+
+/*===========================================
+=            Gulpt default start            =
+===========================================*/
+gulp.task('default', ['copy'], function() {
+
 });
--- index.js
+++ index.js
@@ -1,1 +1,85 @@
 "use strict";
+
+/**
+ * Run objects async
+ *
+ * @param      {obj}    objectAsync  objec to async
+ * @param      {Function}  runfunction  the function to run : ( key,data,cb(err,resolve) )
+ * @param      {Function}  callback     finish callback
+ * 
+ * Example:
+ * 
+ * objAsync(obj, function(key,data,cb){
+ *      var time = key=='key4'? 8000: 2000;
+ *      setTimeout(function(){
+ *          console.log(`${key}: ${data}`);
+ *          cb(null, key);
+ *      },time)
+ * },function(err, resolve){
+ *      console.log('last:',resolve)
+ * })
+ *
+ */
+function objAsync(objectAsync, runfunction, callback) {
+
+    var count = Object.keys(objectAsync).length;
+
+    // loop async
+    function loopasync(cb) {
+
+        for (const key in objectAsync) {
+
+            runfunction(key, objectAsync[key], function(err, resolve) {
+                if (err) return callback(err)
+
+                cb(null, resolve)
+            })
+
+        }
+
+    }
+
+    // Check all async
+    loopasync(function(err, resolve) {
+
+        if (err) return callback(err);
+
+        // total of objects
+        count--;
+
+        // When it is equal to 0 is because all callbacks have returned
+        if (count === 0) callback(null, resolve)
+    })
+}
+
+
+
+// Example
+
+var obj = {
+    key1: 'uno',
+    key2: 'dos',
+    key3: 'tres',
+    key4: 'cuatro'
+}
+
+objAsync(obj, function(key, data, cb) {
+
+    var time = key == 'key4' ? 8000 : 2000;
+
+    setTimeout(function() {
+
+        console.log(`${key}: ${data}`);
+
+        cb(null, key);
+
+    }, time)
+
+
+
+}, function(err, resolve) {
+    
+    if(err) console.log(err);
+
+    console.log('last:', resolve)
+})
